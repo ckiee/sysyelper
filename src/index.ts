2e2d8ec93b5f1907c2a81978e29ae4d1483f7ea4
ckie <git-525ff67@ckie.dev>
2021-09-26 04:42:17 +0300

    ephemeral: init module

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import CookiecordClient from "cookiecord";
 import { Intents } from "discord.js";
 import dotenv from "dotenv-safe";
+import EphemeralModule from "./modules/ephemeral";
 import ProxyManager from "./modules/proxy";
 
 dotenv.config();
 
-const client = new CookiecordClient({
-    botAdmins: process.env.BOT_ADMINS?.split(","),
-    prefix: "yp!"
-}, {
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
-});
+const client = new CookiecordClient(
+    {
+        botAdmins: process.env.BOT_ADMINS?.split(","),
+        prefix: "yp!",
+    },
+    {
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_MEMBERS,
+        ],
+    }
+);
 
 // client.loadModulesFromFolder("src/modules");
 // client.reloadModulesFromFolder("src/modules");
 client.registerModule(ProxyManager);
+client.registerModule(EphemeralModule);
 
 client.login(process.env.TOKEN);
 client.on("ready", () => console.log(`Logged in as ${client.user?.tag}`));
